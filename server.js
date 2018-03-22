861ace785341253fa8a013ad9c67368b1586ac50
camckin10 <chelseymckinney2@gmail.com>
2018-03-21 20:44:25 -0500

    pushing files from chelsey branch

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 var db = require("./models");
-app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use(bodyParser.json({type: 'application/**json'}))
 // app.use(bodyParser.raw({type:'application/vnd.custom-type'}))
 // app.use(bodyParser.text({type:'text/html'}))
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
 
 require("./controllers/htmlRoutes.js")(app);
 require("./controllers/answerRoutes.js");
 require("./controllers/userRoutes.js");
 
 
-db.sequelize.sync({force:true}).then(function(){
-  app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT: " + PORT);
+    });
 });
