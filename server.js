df9d14d43cd76a818bc3b3541cf3324fc14b998e
idaselma3025 <idaselma3025@gmail.com>
2018-03-21 21:16:58 -0500

    s3junk

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
+var fileUpload = require("express-fileupload");
+var s3 = require("s3");
+// var keys = require("./keys.js");
+var fs = require("fs");
+
 
 var app = express();
 
+// var client = s3.createClient({
+// 	maxAsyncS3: 20, // this is the default
+// 	s3RetryCount: 3, // this is the default
+// 	s3RetryDelay: 1000, // this is the default
+// 	multipartUploadThreshold: 20971520, // this is the default (20 MB)
+// 	multipartUploadSize: 15728640, // this is the default (15 MB)
+// 	s3Options: {
+// 		accessKeyId: keys.s3accesskey,
+// 		secretAccessKey: keys.s3secretaccesskey,
+// 		// any other options are passed to new AWS.S3()
+// 		// See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+// 	},
+// });
+
+app.use(fileUpload());
+
 var PORT = process.env.PORT || 8080;
+
 
 var db = require("./models");
 app.use(bodyParser.urlencoded({ extended:false}));
@@ -12,13 +35,23 @@ app.use(bodyParser.urlencoded({ extended:false}));
 // app.use(bodyParser.raw({type:'application/vnd.custom-type'}))
 // app.use(bodyParser.text({type:'text/html'}))
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
 
 require("./controllers/htmlRoutes.js")(app);
 require("./controllers/answerRoutes.js");
 require("./controllers/userRoutes.js");
 
-
+// var client = s3.createClient({
+//   maxAsyncS3: 20,
+//   s3RetryCount: 3,
+//   s3RetryDelay: 1000,
+//   multipartUploadThreshold: 20971520,
+//   multipartUploadSize: 15728640,
+//   s3Options: {
+//     accessKeyId: keys.s3accesskey,
+//     secretAccessKey: keys.s3secretaccesskey
+//   },
+// });
 db.sequelize.sync({force:true}).then(function(){
   app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
